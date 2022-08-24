7b1e995dc1c2fb1c6279ebf65d7eadc9e1f7257e
Cameron Barker <barker.cameron@gmail.com>
2022-08-23 20:33:53 -0700

    init

---
+++ lib/omniauth/instagram_basic.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "instagram_basic/version"
+
+module Omniauth
+  module InstagramBasic
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/omniauth/instagram_basic_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Omniauth::InstagramBasic do
+  it "has a version number" do
+    expect(Omniauth::InstagramBasic::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
